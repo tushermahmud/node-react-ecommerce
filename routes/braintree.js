55f8e01b2ff12994dd52761cfd37dd7ae9e59e58
tushermahmud <56559678+tushermahmud@users.noreply.github.com>
2020-08-04 23:34:55 +0600

    Update braintree.js

--- routes/braintree.js
+++ routes/braintree.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 const {requiredSignin,isAdmin,isAuth}=require('../controllers/auth');
 const {userById}=require('../controllers/user')
-const { generateToken,paymentProcess } = require("../controllers/braintree");
+const { generateToken,paymentProcess } = require("../controllers/brainTree");
 
 router.get("/braintree/getToken/:userId", requiredSignin, isAuth, generateToken);
 router.post("/braintree/payment/:userId",requiredSignin,isAuth,paymentProcess)
